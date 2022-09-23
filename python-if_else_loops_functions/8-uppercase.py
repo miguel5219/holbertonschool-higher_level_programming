cb9e8f0a234e3db874b4c6cd7b60d1b430943033
miguel5219 <5219@holbertonschool.com>
2022-09-22 19:16:48 -0700

    fixed code, fixed PEP8 validation

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-# letter = 0
     for letter in str:
         i = ord(letter)
         _letter = letter
         if i >= 96 and i <= 123:
             _letter = chr(i - 32)
         print("{}".format(_letter), end="")
-#letter += 1
     print("{}".format(""))
-#return True
-
