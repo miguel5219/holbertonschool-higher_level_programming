cf2e33b1ed3db32289575ec11f0098e319ff260f
miguel5219 <5219@holbertonschool.com>
2022-09-30 12:24:21 -0700

    fixed message

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """ class with the size attribute and error messages """
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError("size must be and integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
