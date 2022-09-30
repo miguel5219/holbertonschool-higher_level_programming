ca23f3a45a0a6caa9fcb3e7b1cb73e8ca8dd7f3a
miguel5219 <5219@holbertonschool.com>
2022-09-30 09:49:53 -0700

    fixed code in several lines

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,10 +9,10 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             prod = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
             prod = 0
         except ZeroDivisionError:
-            print("Division by 0")
+            print("division by 0")
             prod = 0
         except IndexError:
             print("out of range")
