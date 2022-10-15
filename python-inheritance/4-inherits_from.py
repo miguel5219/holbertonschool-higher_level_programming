4b69d324b69dfc231d111876e7686a7985077522
miguel5219 <5219@holbertonstudents.com>
2022-10-15 12:04:22 -0500

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module contains the fucntion inherits_from()
+"""
+
+
+def inherits_from(obj, a_class):
+    """ checks the object
+
+    returns: True if the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class
+    """
+
+    if isinstance(obj, a_class) and issubclass(a_class, obj.__class__) is False:
+        return True
+    return False
