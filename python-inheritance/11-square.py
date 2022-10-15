ed2153886b88af291c4a887b9636975e0965ba0f
miguel5219 <5219@holbertonstudents.com>
2022-10-15 15:52:11 -0500

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+contains the class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is a subclass with parent Rectangle
+    """
+    def __init__(self, size):
+        """
+        methodthat validates args and set
+        the as private attributes
+        args:
+            size: Square size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        return: string with information
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
