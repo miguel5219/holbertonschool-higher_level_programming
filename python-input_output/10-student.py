e24b9be752f2faaa03d374fc207adaa09b43169a
miguel5219 <5219@holbertonstudents.com>
2022-10-17 12:46:23 -0500

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" define class method """
+
+
+class Student:
+    """ class """
+    def __init__(self, first_name='', last_name='', age=0):
+        """ method that assigns public attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns: dictionary representation of studente instance 
+        note: if attrs is a list ofstrings, only attribute name
+            contained in this list must be retrieved.
+            otherwise, all attributes must be retrieved
+        """
+
+    obj_ = dict()
+    if type(attrs) is list:
+        for obj_ in attrs:
+            if type(obj_) is not str:
+                return self.__dict__
+
+            if i in self.__dict__:
+                obj_[i] = self.__dict__[i]
+
+        return obj_
+
+    return self.__dict__
