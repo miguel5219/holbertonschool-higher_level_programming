42e1062bee91bb8a1f0dd8b41fee7b85612b71f1
miguel5219 <5219@holbertonstudents.com>
2022-10-17 13:08:16 -0500

    task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,21 +12,21 @@ class Student:
 
     def to_json(self, attrs=None):
         """
-        returns: dictionary representation of studente instance 
+        returns: dictionary representation of studente instance
         note: if attrs is a list ofstrings, only attribute name
             contained in this list must be retrieved.
             otherwise, all attributes must be retrieved
         """
 
-    obj_ = dict()
-    if type(attrs) is list:
-        for obj_ in attrs:
-            if type(obj_) is not str:
-                return self.__dict__
+        obj_ = dict()
+        if type(attrs) is list:
+            for obj_ in attrs:
+                if type(obj_) is not str:
+                    return self.__dict__
 
-            if i in self.__dict__:
-                obj_[i] = self.__dict__[i]
+                if i in self.__dict__:
+                    obj_[i] = self.__dict__[i]
 
-        return obj_
+            return obj_
 
-    return self.__dict__
+        return self.__dict__
