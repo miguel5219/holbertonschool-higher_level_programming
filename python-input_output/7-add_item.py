94ab8fba734de4a6283dfbb6ac00eb35815199be
miguel5219 <5219@holbertonstudents.com>
2022-10-17 10:59:02 -0500

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """ module imports two functions and execute """
 
-
-import sys
+from os import path
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_obj_json = "add_item.json"
+if path.exists("add_item.json") is False:
+    save_to_json_file([], "add_item.json")
+_list = load_from_json_file("add_item.json")
 
-try:
-    _list = load_from_json_file(file_obj_json)
-except FileNotFoundError:
-    _list = []
-
-for element in sys.argv[1:]:
-    _list.append(element)
-
-save_to_json_file(_list, file_obj_json)
+for element in range(1, len(argv)):
+    _list.append(argv[element])
+save_to_json_file(_list, "add_item.json")
