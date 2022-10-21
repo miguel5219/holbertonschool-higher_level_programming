d2721514367557bc180377f3c8dd757b7196c909
miguel5219 <5219@holbertonstudents.com>
2022-10-21 18:00:34 -0500

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module contains the class Rectangle """
+
+
+from models.base import base
+
+
+class Rectangle(Base):
+    """ class represents a rectangle and inherits from Base
+
+    attributes:
+        width: the width of the rectangle
+        height: the height of the rectangle
+        x: the x coordinate of the rectangle
+        y: the y coordinate of the rectangle
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
