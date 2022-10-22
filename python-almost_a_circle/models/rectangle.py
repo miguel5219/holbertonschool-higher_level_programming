9401674ace0316a12d917926e9aa3cf4cd1703cb
miguel5219 <5219@holbertonstudents.com>
2022-10-21 19:17:39 -0500

    .

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,17 +17,18 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
 
-        super(),__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super(),__init__(id)
 
     @property
     def width(self):
         return self.__width
 
     @width.setter
+    p
     def width(self, value):
         if type(value) != int:
             raise TypeError('width must be an integer')
