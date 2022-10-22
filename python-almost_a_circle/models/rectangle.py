2a87c5049e897a4480833492cc8080c2bf972043
miguel5219 <5219@holbertonstudents.com>
2022-10-22 16:59:47 -0500

    .

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,36 @@ class Rectangle(Base):
         """ returns the string of the object """
         return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}" \
             f" - {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        """ assings an argument toeach attribute
+
+        attribute:
+            args: attributes to be update in the following
+            order
+
+            1st argument should be the id attribute
+            2nd argument should be the width attribute
+            3rd argument should be the height attribute
+            4th argument should be the x attribute
+            5th argument should be the y attribute
+
+            kwargs: dictionary with the name of the attrs as
+            keys and the values to change associated
+        """
+
+    for count, arg in enumerate(args):
+        if count == 0:
+            self.id = arg
+        elif count == 1:
+            self.width = arg
+        elif count == 2:
+            self.height = arg
+        elif count == 3:
+            self.x = arg
+        elif count == 4:
+            self.y = arg
+
+    if not args:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
