94441fe7f3b36c32f0e8b8dc087e2be8fcf3b624
miguel5219 <5219@holbertonstudents.com>
2022-10-23 07:42:27 -0500

    .

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" module contains the class square """
+
+
+from models.rectangle import rectangle
+
+
+class Square(Rectangle):
+    """ class represents a square inherits from rectangle
+
+    attributes:
+        width: the size of the square
+        height: the size of the square
+        x: the x coordinate of the square
+        y: the y coordinate of the square
+        id: identifier of the square
+    """
+
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def update(self, *args, **kwargs):
+        """ give an argument to each attribute
+
+        attributes:
+            args: contains the attributes to be
+            updates in the following order
+
+        kwargs: a dictionary with the name of the
+        attrs as keys and the values to change
+        associated
+        """
+
+    if len(args):
+        for i, a in enumerate(args):
+            if i == 0:
+                self.id = a
+            elif i == 1:
+                self.width = a
+            elif i == 2:
+                self.height = a
+            elif i == 3:
+                self.x = a
+            elif i == 4:
+                self.y = a
+
+    if not args:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}" \
+                f" - {self.width}"
