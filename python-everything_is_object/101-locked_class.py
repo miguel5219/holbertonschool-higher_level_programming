5d9355bed5d8f1d12015e4b9ef5773d184078ccd
miguel5219 <5219@holbertonstudents.com>
2022-11-16 15:54:47 -0500

    .

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module contains the class LockedClass """
+
+
+class LockedClass:
+    """ class only allows to assign attributes with
+    the name first_name """
+
+    __slots__ =['first_name']
