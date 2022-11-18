8cffd880a646393effc4e45a6cfea03a7a41eab5
miguel5219 <5219@holbertonstudents.com>
2022-11-18 14:39:41 -0500

    task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- database hbtn_0d_tvshows.
 SELECT t1.title, t3.name
 FROM tv_shows t1
-LEFT JOIN tv_show genres t2
+LEFT JOIN tv_show_genres t2
 ON t1.id = t2.show_id
 LEFT JOIN tv_genres t3
 ON t2.genre_id = t3.id
