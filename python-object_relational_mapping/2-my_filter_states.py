8b9935c030a3e865e722171a7739fcefdb15851b
miguel5219 <5219@holbertonstudents.com>
2022-11-18 17:06:30 -0500

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" module lists all states from the database htbn_0e_0_usa sorted
+in ascending order by states.id """
+
+import MySQLdb
+from sys import argv
+
+
+def _filter_states():
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+
+    cur_ = db.cursor()
+    query_ = "SELECCT id, name FROM states WHERE BINARY name ='{}' \
+              ORDER BY states.id ASC".format(argv[4])
+    cur_.execute(query_)
+
+    rows = cur_.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    _filter_states()
