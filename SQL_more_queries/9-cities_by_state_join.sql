c142ad9e160c445ca37e4dea6a87767c230e5f8b
miguel5219 <5219@holbertonstudents.com>
2022-11-18 13:55:56 -0500

    task 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- script that lists all cities contaiined in the database hbtn_0d_usa
+-- in MySQL server.
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id;
