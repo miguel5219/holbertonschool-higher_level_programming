4f19cb81a79afdaa1e6d8065a9b19c2a09643edc
miguel5219 <5219@holbertonstudents.com>
2022-11-19 18:02:45 -0500

    task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" module contains the  function model_state_my_get() """
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state_my_get():
+    """ prints the state object with the named passed as
+    argument from the database """
+
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                      sys.argv[2],
+                                                      sys.argv[3])
+    
+    if ';' in argv[4]:
+        return
+
+    engine_ = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine_)
+    State.metadata.create_all(engine_)
+
+    Session = sessionmaker(bind=engine_)
+
+    conn = engine_.connect()
+    session = Session(bind=conn)
+
+    objs = session.query(State).order_by(State.id).all()
+
+    for obj in objs:
+        if sys.argv[4] == obj.name:
+            print(obj.id)
+            return
+    print('Not found')
+
+
+if __name__ == "__main__":
+    model_state_my_get()
