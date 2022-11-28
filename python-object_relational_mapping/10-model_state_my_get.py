88bc459a9a7199a54890634bf068eb51f5c6f4a9
miguel5219 <5219@holbertonstudents.com>
2022-11-28 12:25:09 -0500

    update

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
 """ module contains the  function model_state_my_get() """
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
-
-
-def model_state_my_get():
-    """ prints the state object with the named passed as
-    argument from the database """
-
-    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                      sys.argv[2],
-                                                      sys.argv[3])
-
-    if ';' in sys.argv[4]:
-        return
-
-    engine_ = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine_)
-    State.metadata.create_all(engine_)
-
-    Session = sessionmaker(bind=engine_)
-
-    conn = engine_.connect()
-    session = Session(bind=conn)
-
-    objs = session.query(State).order_by(State.id).all()
-
-    for obj in objs:
-        if sys.argv[4] == obj.name:
-            print(obj.id)
-            return
-    print('Not found')
-
-
 if __name__ == "__main__":
-    model_state_my_get()
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    import sys
+    search = False
+
+
+    engine_ = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine_.connect()
+
+    session = Session(engine)
+
+    for state in session.query(State).all():
+    if sys.argv[4] == state.name:
+        print("{}".format(state.id))
+        search = True
+    if search is False:
+        print("Not found")
+    session.close()
