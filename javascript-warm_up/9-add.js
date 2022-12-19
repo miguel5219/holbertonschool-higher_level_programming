84bcf1a6be7300792337ec30696aa563db1bc90f
miguel5219 <5219@holbertonstudents.com>
2022-12-19 17:27:17 -0500

    script that prints the addition of 2 integers

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argument = process.argv;
+const argument1 = parseInt(args[2]);
+const argument2 = parseInt(args[3]);
+function add (argument1, argument2) {
+  return argument1 + argument2;
+}
+console.log(add(argument1, argument2));
