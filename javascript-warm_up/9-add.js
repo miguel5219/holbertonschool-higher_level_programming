d8ec6a952efee0f308f46680648153e5dc3dcfce
miguel5219 <5219@holbertonstudents.com>
2022-12-19 17:28:53 -0500

    fixed code

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argument = process.argv;
-const argument1 = parseInt(args[2]);
-const argument2 = parseInt(args[3]);
+const argument1 = parseInt(argument[2]);
+const argument2 = parseInt(argument[3]);
 function add (argument1, argument2) {
   return argument1 + argument2;
 }
