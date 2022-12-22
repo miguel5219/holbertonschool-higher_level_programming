29f3c8901bef246d56a34a4483e1dd8e7767fc77
miguel5219 <5219@holbertonstudents.com>
2022-12-22 17:07:30 -0500

    script that display the status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(`code: $(response.statusCode)`);
+    console.log(`code: ${response.statusCode}`);
   }
 });
