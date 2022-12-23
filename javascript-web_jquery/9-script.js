5e639f8022bd14f72a78b89166680fff1d7c1ede
miguel5219 <5219@holbertonstudents.com>
2022-12-23 16:19:36 -0500

    script that fetches from https://stefanbohacek.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://stefanbohacek.com/hellosalut/?lang=fr',
+        success: function(input) {
+            $("DIV#hello").text(input.hello);
+        }
+    });
+});
