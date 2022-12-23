b94a85fd4faa5ee6892a7f1e206a65dfd57d2056
miguel5219 <5219@holbertonstudents.com>
2022-12-22 21:17:19 -0500

    script that computes the number of tasks completed by user id.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const urlApi = process.argv[2];
+request(urlApi, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const file = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed === True) {
+      if (file[task.userId]) {
+        file[task.userId]++;
+      } else {
+        file[task.userId] = 1;
+      }
+    }
+  }
+  console.log(file);
+});
